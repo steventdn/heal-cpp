728fade6679e14b1173aac55dfa0e79bd59fab35
steventdn <stevennguy20@gmail.com>
2023-11-20 23:11:28 -0800

    removed atlas.js

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const User = require("./mongo"); // Import the User model
 const cors = require("cors");
-const connectDB = require("./atlas"); 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
