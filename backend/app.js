94a1aedfadff63e9ace739282b86da2699df0ea0
steventdn <stevennguy20@gmail.com>
2023-12-08 18:16:59 -0800

    env changes

--- backend/app.js
+++ backend/app.js
@@ -5,16 +5,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Specify the allowed origin of your React app
-const corsOptions = {
+/*const corsOptions = {
   origin: 'https://main.d1ieej04guld9e.amplifyapp.com',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Include credentials (cookies, HTTP authentication) in the CORS request
   optionsSuccessStatus: 204, // Return a 204 status for preflight requests
 };
-
+*/
 // Use the cors middleware with the specified options
-app.use(cors(corsOptions));
-
+//app.use(cors(corsOptions));
+app.use(cors());
 app.get("/", cors(), (req, res) => {
   res.send("Hello, this is your Express server!");
 });
